6bebd5de8650a2c16dcc8ebf129eefb8265feecd
Az4reus <az4reus@gmail.com>
2016-05-11 11:08:37 +0200

    Database connection rewrites, extra functions

--- database.py
+++ database.py
@@ -7,11 +7,21 @@ import time
 import csv_reading
 
 
-def init_db() -> sq.Connection:
+def init_db():
     c = sq.connect('./alcohol_study.db')
     cur = c.cursor()
 
-    cur.executescript("""
+    table_query = "SELECT name FROM sqlite_master WHERE type='table';"
+
+    if not cur.execute(table_query).fetchall():
+        set_up_tables(c)
+
+    return c
+
+
+def set_up_tables(conn):
+    c = conn.cursor()
+    c.executescript("""
     CREATE TABLE IF NOT EXISTS pictures (
       name TEXT,
       link BLOB,
@@ -21,6 +31,7 @@ def init_db() -> sq.Connection:
     );
 
     CREATE TABLE IF NOT EXISTS picture_evaluation_data (
+      id INTEGER PRIMARY KEY,
       picture_name TEXT,
       shows_people BOOLEAN,
       focused_people INTEGER,
@@ -50,8 +61,8 @@ def init_db() -> sq.Connection:
       answers BLOB
     );
     """)
-    c.commit()
-    return c
+
+    conn.commit()
 
 
 def get_next_picture():
@@ -59,7 +70,7 @@ def get_next_picture():
     cur = c.cursor()
 
     cur.execute("""
-    SELECT link, username FROM pictures
+    SELECT link, username, ROWID FROM pictures
     WHERE evaluated = 0
     AND name != ''
     LIMIT 1;
@@ -73,7 +84,7 @@ def get_next_user_id_picture(id):
     cur = c.cursor()
 
     cur.execute('''
-    SELECT link FROM pictures
+    SELECT link, ROWID FROM pictures
     WHERE evaluated = 0
     AND name != ''
     AND pictures.username = ?
@@ -83,12 +94,12 @@ def get_next_user_id_picture(id):
     return cur.fetchall()
 
 
-def insert_picture(c: sq.Connection,
+def insert_picture(c,
                    name: str,
                    link: str,
                    username: str,
                    evaluated: bool = False,
-                   obsolete: bool = False):
+                   finished: bool = False):
     cursor = c.cursor()
 
     try:
@@ -96,14 +107,14 @@ def insert_picture(c: sq.Connection,
                            [name]).fetchall()[0][0]
 
     except IndexError:
-
         cursor.execute("INSERT INTO pictures VALUES (?, ?, ?, ?, ?);",
                        [name,
                         link,
                         1 if evaluated else 0,
                         username,
-                        1 if obsolete else 0])
+                        1 if finished else 0])
 
+    cursor.close()
     c.commit()
 
 
@@ -116,18 +127,20 @@ def count_unevaluated_pictures():
         .fetchall()[0][0]
 
 
-def insert_picture_data(form):
+def insert_picture_eval_data(form):
     shows_people = True if form['containsPeople'] == 'Yes' else False
     picture_name = form['picture_name']
     focused_people = form['focalSubjects']
     nonfocused_people = form['nonFocalSubjects']
+    identifier = form['picture_id']
 
     c = init_db()
     cur = c.cursor()
 
     cur.execute("""
-    INSERT INTO picture_evaluation_data VALUES (?, ?, ?, ?)
-    """, [picture_name, shows_people, focused_people, nonfocused_people])
+    INSERT INTO picture_evaluation_data VALUES (?, ?, ?, ?, ?)
+    """, [identifier, picture_name, shows_people, focused_people,
+          nonfocused_people])
 
     c.commit()
 
@@ -173,6 +186,7 @@ def get_next_relevant_picture_for_user(user_id):
               ON pictures.link = picture_evaluation_data.picture_name
             WHERE pictures.username=?
             AND picture_evaluation_data.shows_people = 1
+            AND pictures.finished = 0
             LIMIT 1""",
             [user_id]).fetchall()
 
@@ -236,6 +250,30 @@ def get_evaluations_left(picture_name):
     return focal_subjects - left
 
 
+def get_picture_by_id(id):
+    """Fetches any given picture by its rowID, effectively enumerating
+    all pictures."""
+    conn = init_db()
+    cur = conn.cursor()
+
+    return cur.execute('''
+    SELECT * FROM pictures WHERE ROWID = ?
+    ''', [id]).fetchall()[0]
+
+
+def get_row_by_link(picture_link):
+    """Fetches the given row ID in the pictures table by link.
+    :param picture_link: Metricwire link to said picture.
+    """
+
+    conn = init_db()
+    cur = conn.cursor()
+
+    return cur.execute('''
+    SELECT rowID FROM pictures WHERE link = ?
+    ''', [picture_link]).fetchall()[0][0]
+
+
 def save_focal_survey_result(f):
     conn = init_db()
     cur = conn.cursor()
@@ -284,24 +322,28 @@ def save_nf_survey_result(f, unfocused_people):
     conn = init_db()
     cur = conn.cursor()
 
-    dict = f.to_dict()
-    if not 'unfocusedPeople' in f:
-        return
+    d = f.to_dict()
+    d['unfocusedPeople'] = unfocused_people
+    picture = d['picture_name']
 
-    dict['unfocusedPeople'] = unfocused_people
-    picture = dict['picture_name']
-
-    j = json.dumps(dict)
+    j = json.dumps(d)
 
     cur.execute('''
     INSERT INTO picture_non_focal_result_data VALUES (?, ?)
     ''', [picture, j])
-
     conn.commit()
+
+    set_done(picture)
+
+
+def set_done(picture):
+    conn = init_db()
+    cur = conn.cursor()
 
     cur.execute('''
     UPDATE pictures SET finished = 1 WHERE link=?
     ''', [picture])
+    conn.commit()
 
 
 def drop_database():
