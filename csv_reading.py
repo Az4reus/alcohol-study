d893b65f6416b8a208eaecbdce1f06fb39497788
Az4reus <az4reus@gmail.com>
2016-05-13 19:31:39 +0200

    Adds Excel-CSV support.

--- csv_reading.py
+++ csv_reading.py
@@ -6,14 +6,25 @@ def read_data_csv(path_to_csv: str):
     urlrow = 'Take a picture of your environment right now. Take a picture of what you see.'
     c = db.init_db()
 
-    with open(path_to_csv, encoding='utf-8-sig') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            db.insert_picture(
-                    c,
-                    extract_name_from_url(row[urlrow]),
-                    row[urlrow],
-                    row['User Id'])
+    try:
+        with open(path_to_csv, encoding='utf-8-sig') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                db.insert_picture(
+                        c,
+                        extract_name_from_url(row[urlrow]),
+                        row[urlrow],
+                        row['User Id'])
+                
+    except UnicodeDecodeError:
+        with open(path_to_csv, encoding='iso-8859-1') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                db.insert_picture(
+                        c,
+                        extract_name_from_url(row[urlrow]),
+                        row[urlrow],
+                        row['User Id'])
 
 
 def extract_name_from_url(picture_url: str) -> str:
