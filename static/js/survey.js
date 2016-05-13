c56fbdbdd6dfd14c54f0f126aa124c288f8446a2
Az4reus <az4reus@gmail.com>
2016-05-13 19:08:26 +0200

    Fixes multiple text-box issue.

--- static/js/survey.js
+++ static/js/survey.js
@@ -4,15 +4,19 @@
 
 var addTextbox = function (id) {
     var e = document.getElementById(id).parentNode;
+    var textbox = document.getElementById(id + '_textbox');
 
-    var label = document.createElement('label');
-    var text = document.createTextNode('Please specify:       ');
-    label.appendChild(text);
+    if (textbox == null) {
+        var label = document.createElement('label');
+        var text = document.createTextNode('Please specify:       ');
+        label.appendChild(text);
 
-    var box = document.createElement("input");
-    box.setAttribute('type', 'text');
-    box.setAttribute('name', id+'_textbox');
+        var box = document.createElement("input");
+        box.setAttribute('type', 'text');
+        box.setAttribute('name', id + '_textbox');
+        box.setAttribute('id', id + '_textbox');
 
-    label.appendChild(box);
-    e.appendChild(label);
+        label.appendChild(box);
+        e.appendChild(label);
+    }
 };
